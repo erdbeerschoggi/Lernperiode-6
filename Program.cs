dae10dd4096f38fd1a0e4c220537e5a2d69e2344
erdbeerschoggi <M.Piotrowska.inf24@stud.bbbaden.ch>
2025-08-22 11:49:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Paladins
+    {
+        public Paladins()
+        {
+            Paladins Lance = new Paladins();
+            Paladins Keith = new Paladins();
+            Paladins Pidge = new Paladins();
+            Paladins Hunk = new Paladins();
+            Paladins Shiro = new Paladins();
+        }
+    }
+
+    public class ZarkonsFleet 
+    {
+        public ZarkonsFleet()
+        {
+            ZarkonsFleet Zarkon = new ZarkonsFleet();
+            ZarkonsFleet Haggar = new ZarkonsFleet();
+            ZarkonsFleet Sendak = new ZarkonsFleet();
+            ZarkonsFleet Lotor = new ZarkonsFleet();
+        }
+
+    }
+
+
+}
