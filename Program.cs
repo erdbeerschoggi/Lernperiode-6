c0c4375335c7b9022ecab8cb76a1d6e32e3fde83
erdbeerschoggi <M.Piotrowska.inf24@stud.bbbaden.ch>
2025-09-05 11:51:21 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,84 @@
-﻿namespace RPG
+﻿using static RPG.Program;
+
+namespace RPG
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+          
+        }
+
+
+        public class Paladins 
+        {
+            public string Name { get; set; }
+            public int Health { get; set; }
+            public string TeamRole { get; set; }
+            public string Lion { get; set; }
+            public string Weapon { get; set; }
+
+
+            public Paladins(string name, int health, string TeamRole, string lion, string Weapon) 
+            {
+                Paladins Lance = new Paladins("Lance", 100, "Team builder", "Blue Lion", "Sniperrifle");
+                Paladins Keith = new Paladins("Keith", 100, "Right Hand Man", "Red Lion", "EnergySword");
+                Paladins Shiro = new Paladins("Shiro", 100, "Leader", "Black Lion", "Galra Tech Arm");
+                Paladins Pidge = new Paladins("Pidge", 100, "The Brains", "Green Lion", "Electric Grappling Hook");
+                Paladins Hunk = new Paladins("Hunk", 100, "Engineer", "Yellow Lion", "EnergyGun");
+                Paladins Allura = new Paladins("Allura", 100, "Princess", "Blue Lion", "AlteanAlchemy"); //Maybe remove her if the code gets to complicated, because shes not that important for the basic game
+
+            }
             
         }
 
-        private class Paladins
+        public class ZarkonsFleet
         {
-            Paladins Lance = new Paladins();
-            Paladins Keith = new Paladins();
-            Paladins Shiro = new Paladins();
-            Paladins Pidge = new Paladins();
-            Paladins Hunk = new Paladins();
-            Paladins Allura = new Paladins();
+            public string Name { get; set; }
+            public int Health { get; set; }
+            public string TeamRole { get; set; }
+            public string Weapon { get; set; }
+
+            public ZarkonsFleet(string Name, int Health, string TeamRole, string Weapon) 
+            {
+                ZarkonsFleet Zarkon = new ZarkonsFleet("Zarkon", 100, "Emperor", "GalraTech");
+                ZarkonsFleet Lotor = new ZarkonsFleet("Lotor", 100, "Prince", "Sword");
+                ZarkonsFleet Haggar = new ZarkonsFleet("Haggar", 100, "Right Hand Man", "AlteanAlchemy");
+                ZarkonsFleet Sendak = new ZarkonsFleet("Sendak", 100, "Part of Zarkons Fleet", "Army");
+            }
+            
         }
 
-        private class ZarkonsFleet
+        public class Weapons
         {
-            ZarkonsFleet Zarkon = new ZarkonsFleet();
-            ZarkonsFleet Lotor = new ZarkonsFleet();
-            ZarkonsFleet Haggar = new ZarkonsFleet();
-            ZarkonsFleet Sendak = new ZarkonsFleet();
+            public string WeaponName { get; set;}
+            public string Ability { get; set;}
+
+            public Weapons(string WeaponName, string Ability)
+            {
+                Weapons SniperRifle = new Weapons("Sniper Rifle", "Shooting precisely");
+                Weapons ElectricGrapplingHook = new Weapons("GrapplingHook", "has a High Voltage Shock");
+                Weapons EnergyGun = new Weapons("Energy Gun", "Shooting with alot of strength");
+                Weapons EnergySword = new Weapons("Energy Sword", "Sword for close combat");
+                Weapons GalraTechArm = new Weapons("Galra Tech Arm", "Energy blades and blasters");
+                Weapons AlteanAlchemy = new Weapons("Altean Alchemy", "Healing");
+            }
         }
 
-        private class Weapons
+        public class Lions
         {
-            Weapons SniperRifle = new Weapons();
-            Weapons GrapplingHook = new Weapons();
-            Weapons EnergyGun = new Weapons();
-            Weapons EnergySword = new Weapons();
-            Weapons GalraTechArm = new Weapons();
+            public string Name { get; set;}
+            public string Lioncolour { get; set;}
+            public string LionAbilitys { get; set;}
 
-        }
-
-        private class Lions
-        {
-            Lions BlueLion = new Lions();
-            Lions RedLion = new Lions();
-            Lions YellowLion = new Lions();
-            Lions GreenLion = new Lions();
-            Lions BlackLion = new Lions();
+            public Lions(string Name, string Lioncolour, string LionAbility)
+            {
+                Lions BlueLion = new Lions("Blue Lion", "Blue", "Ice Rays");
+                Lions RedLion = new Lions("Red Lion", "Red", "Electric Rays");
+                Lions YellowLion = new Lions("Yellow Lion","Yellow", "Extra Strength - superior Armor");
+                Lions GreenLion = new Lions("Green Lion", "Green", "Plant Rays");
+                Lions BlackLion = new Lions("Black Lion", "Black", "Wings - Extra strength and speed");
+            }
         }
 
     }
