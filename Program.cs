65677a28fa613f04e8626364f65765700eeddb86
erdbeerschoggi <M.Piotrowska.inf24@stud.bbbaden.ch>
2025-09-11 21:34:50 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,7 +6,70 @@ namespace RPG
     {
         static void Main(string[] args)
         {
-          
+            //Paladins
+            Paladins Lance = new Paladins("Lance", 100, "Team builder", "Blue Lion", "Sniperrifle");
+            Paladins Keith = new Paladins("Keith", 100, "Right Hand Man", "Red Lion", "EnergySword");
+            Paladins Shiro = new Paladins("Shiro", 100, "Leader", "Black Lion", "Galra Tech Arm");
+            Paladins Pidge = new Paladins("Pidge", 100, "The Brains", "Green Lion", "Electric Grappling Hook");
+            Paladins Hunk = new Paladins("Hunk", 100, "Engineer", "Yellow Lion", "EnergyGun");
+            Paladins Allura = new Paladins("Allura", 100, "Princess", "Blue Lion", "AlteanAlchemy"); //Maybe remove her if the code gets to complicated, because shes not that important for the basic game
+
+            //Zarkons Fleet
+            ZarkonsFleet Zarkon = new ZarkonsFleet("Zarkon", 100, "Emperor", "GalraTech");
+            ZarkonsFleet Lotor = new ZarkonsFleet("Lotor", 100, "Prince", "Sword");
+            ZarkonsFleet Haggar = new ZarkonsFleet("Haggar", 100, "Right Hand Man", "AlteanAlchemy");
+            ZarkonsFleet Sendak = new ZarkonsFleet("Sendak", 100, "Part of Zarkons Fleet", "Army");
+
+            //Weapons
+            Weapons SniperRifle = new Weapons("Sniper Rifle", "Shooting precisely");
+            Weapons ElectricGrapplingHook = new Weapons("GrapplingHook", "has a High Voltage Shock");
+            Weapons EnergyGun = new Weapons("Energy Gun", "Shooting with alot of strength");
+            Weapons EnergySword = new Weapons("Energy Sword", "Sword for close combat");
+            Weapons GalraTechArm = new Weapons("Galra Tech Arm", "Energy blades and blasters");
+            Weapons AlteanAlchemy = new Weapons("Altean Alchemy", "Healing");
+
+            //Lions
+            Lions BlueLion = new Lions("Blue Lion", "Blue", "Ice Rays");
+            Lions RedLion = new Lions("Red Lion", "Red", "Electric Rays");
+            Lions YellowLion = new Lions("Yellow Lion", "Yellow", "Extra Strength - superior Armor");
+            Lions GreenLion = new Lions("Green Lion", "Green", "Plant Rays");
+            Lions BlackLion = new Lions("Black Lion", "Black", "Wings - Extra strength and speed");
+
+
+            //The Code
+            // Liste aller Paladins
+            List<Paladins> paladins = new List<Paladins>
+            {
+                Lance, Keith, Shiro, Pidge, Hunk, Allura
+            };
+
+            // Menü anzeigen
+            Console.WriteLine("Wähle einen Paladin aus:");
+            for (int i = 0; i < paladins.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {paladins[i].Name}");
+            }
+
+            // Eingabe lesen
+            Console.Write("Deine Auswahl: ");
+            int auswahl = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            // Gewählten Paladin + Attribute anzeigen
+            if (auswahl >= 0 && auswahl < paladins.Count)
+            {
+                Paladins selected = paladins[auswahl];
+
+                Console.WriteLine("\n--- Gewählter Paladin ---");
+                Console.WriteLine($"Name:   {selected.Name}");
+                Console.WriteLine($"Health: {selected.Health}");
+                Console.WriteLine($"Role:   {selected.Teamrole}");
+                Console.WriteLine($"Lion:   {selected.Lion}");
+                Console.WriteLine($"Weapon: {selected.Weapon}");
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Auswahl!");
+            }
         }
 
 
@@ -14,19 +77,18 @@ namespace RPG
         {
             public string Name { get; set; }
             public int Health { get; set; }
-            public string TeamRole { get; set; }
+            public string Teamrole { get; set; }
             public string Lion { get; set; }
             public string Weapon { get; set; }
 
 
-            public Paladins(string name, int health, string TeamRole, string lion, string Weapon) 
+            public Paladins(string name, int health, string teamrole, string lion, string weapon) 
             {
-                Paladins Lance = new Paladins("Lance", 100, "Team builder", "Blue Lion", "Sniperrifle");
-                Paladins Keith = new Paladins("Keith", 100, "Right Hand Man", "Red Lion", "EnergySword");
-                Paladins Shiro = new Paladins("Shiro", 100, "Leader", "Black Lion", "Galra Tech Arm");
-                Paladins Pidge = new Paladins("Pidge", 100, "The Brains", "Green Lion", "Electric Grappling Hook");
-                Paladins Hunk = new Paladins("Hunk", 100, "Engineer", "Yellow Lion", "EnergyGun");
-                Paladins Allura = new Paladins("Allura", 100, "Princess", "Blue Lion", "AlteanAlchemy"); //Maybe remove her if the code gets to complicated, because shes not that important for the basic game
+                Name = name;
+                Health = health;
+                Teamrole = teamrole;
+                Lion = lion;
+                Weapon = weapon;
 
             }
             
@@ -36,15 +98,16 @@ namespace RPG
         {
             public string Name { get; set; }
             public int Health { get; set; }
-            public string TeamRole { get; set; }
+            public string Teamrole { get; set; }
             public string Weapon { get; set; }
 
-            public ZarkonsFleet(string Name, int Health, string TeamRole, string Weapon) 
+            public ZarkonsFleet(string name, int health, string teamrole, string weapon) 
             {
-                ZarkonsFleet Zarkon = new ZarkonsFleet("Zarkon", 100, "Emperor", "GalraTech");
-                ZarkonsFleet Lotor = new ZarkonsFleet("Lotor", 100, "Prince", "Sword");
-                ZarkonsFleet Haggar = new ZarkonsFleet("Haggar", 100, "Right Hand Man", "AlteanAlchemy");
-                ZarkonsFleet Sendak = new ZarkonsFleet("Sendak", 100, "Part of Zarkons Fleet", "Army");
+                Name = name;
+                Health = health;
+                Teamrole = teamrole;
+                Weapon = weapon;
+                
             }
             
         }
@@ -54,14 +117,10 @@ namespace RPG
             public string WeaponName { get; set;}
             public string Ability { get; set;}
 
-            public Weapons(string WeaponName, string Ability)
+            public Weapons(string weaponName, string ability)
             {
-                Weapons SniperRifle = new Weapons("Sniper Rifle", "Shooting precisely");
-                Weapons ElectricGrapplingHook = new Weapons("GrapplingHook", "has a High Voltage Shock");
-                Weapons EnergyGun = new Weapons("Energy Gun", "Shooting with alot of strength");
-                Weapons EnergySword = new Weapons("Energy Sword", "Sword for close combat");
-                Weapons GalraTechArm = new Weapons("Galra Tech Arm", "Energy blades and blasters");
-                Weapons AlteanAlchemy = new Weapons("Altean Alchemy", "Healing");
+                WeaponName = weaponName;
+                Ability = ability;
             }
         }
 
@@ -71,13 +130,11 @@ namespace RPG
             public string Lioncolour { get; set;}
             public string LionAbilitys { get; set;}
 
-            public Lions(string Name, string Lioncolour, string LionAbility)
+            public Lions(string name, string lioncolour, string lionAbility)
             {
-                Lions BlueLion = new Lions("Blue Lion", "Blue", "Ice Rays");
-                Lions RedLion = new Lions("Red Lion", "Red", "Electric Rays");
-                Lions YellowLion = new Lions("Yellow Lion","Yellow", "Extra Strength - superior Armor");
-                Lions GreenLion = new Lions("Green Lion", "Green", "Plant Rays");
-                Lions BlackLion = new Lions("Black Lion", "Black", "Wings - Extra strength and speed");
+                Name = name;
+                Lioncolour = lioncolour;
+                LionAbilitys = lionAbility;
             }
         }
 
